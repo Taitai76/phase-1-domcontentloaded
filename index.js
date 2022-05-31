9bb35c2572d5bc525055ddf046a237d11151c6c5
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-31 15:52:35 -0600

    commit tree 33cc1379605b7a04f888d401cfcd1e3ae866a5b8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+
+    const word = document.getElementById("text");
+    word.innerHTML = "This is really cool!";
+    console.log(word);
+
+  });
+  
